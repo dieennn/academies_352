8d26e118534e44eca75fcfec60a56bab362c0fd3
Dien <dien_kamal@ymail.com>
2022-09-20 21:03:58 +0700

    Latihan MediaPlayer dengan Service & Notification

--- MyMediaPlayer/app/src/main/java/com/example/mymediaplayer/MainActivity.kt
+++ MyMediaPlayer/app/src/main/java/com/example/mymediaplayer/MainActivity.kt
@@ -1,15 +1,42 @@
 package com.example.mymediaplayer
 
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
 import android.media.AudioAttributes
 import android.media.MediaPlayer
+import android.os.*
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import android.widget.Button
 import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
-    private var mMediaPlayer: MediaPlayer? = null
-    private var isReady: Boolean = false
+
+    companion object {
+        const val TAG = "MainActivity"
+    }
+
+    private var mService: Messenger? = null
+
+    private lateinit var mBoundServiceIntent: Intent
+    private var mServiceBound = false
+
+    /*
+    Service Connection adalah interface yang digunakan untuk menghubungkan antara boundservice dengan activity
+     */
+    private val mServiceConnection = object : ServiceConnection {
+        override fun onServiceDisconnected(name: ComponentName) {
+            mService = null
+            mServiceBound = false
+        }
+
+        override fun onServiceConnected(name: ComponentName, service: IBinder) {
+            mService = Messenger(service)
+            mServiceBound = true
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,43 +45,71 @@ class MainActivity : AppCompatActivity() {
         val btnPlay = findViewById<Button>(R.id.btn_play)
         val btnStop = findViewById<Button>(R.id.btn_stop)
         btnPlay.setOnClickListener {
-            if (!isReady) {
-                mMediaPlayer?.prepareAsync()
-            } else {
-                if (mMediaPlayer?.isPlaying as Boolean) {
-                    mMediaPlayer?.pause()
-                } else {
-                    mMediaPlayer?.start()
+            /*
+            Untuk mengirim perintah play
+            */
+            if (mServiceBound) {
+                try {
+                    mService?.send(Message.obtain(null, MediaService.PLAY, 0, 0))
+                } catch (e: RemoteException) {
+                    e.printStackTrace()
                 }
             }
         }
         btnStop.setOnClickListener {
-            if (mMediaPlayer?.isPlaying as Boolean || isReady) {
-                mMediaPlayer?.stop()
-                isReady = false
+            /*
+            Untuk mengirim perintah stop
+            */
+            if (mServiceBound) {
+                try {
+                    mService?.send(Message.obtain(null, MediaService.STOP, 0, 0))
+                } catch (e: RemoteException) {
+                    e.printStackTrace()
+                }
             }
         }
 
-        init()
+        /*
+        Start service untuk media player
+        */
+        mBoundServiceIntent = Intent(this@MainActivity, MediaService::class.java)
+        mBoundServiceIntent.action = MediaService.ACTION_CREATE
+
+        startService(mBoundServiceIntent)
+        bindService(mBoundServiceIntent, mServiceConnection, Context.BIND_AUTO_CREATE)
     }
 
-    private fun init() {
-        mMediaPlayer = MediaPlayer()
-        val attribute = AudioAttributes.Builder()
-            .setUsage(AudioAttributes.USAGE_MEDIA)
-            .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
-            .build()
-        mMediaPlayer?.setAudioAttributes(attribute)
-        val afd = applicationContext.resources.openRawResourceFd(R.raw.keane_everybodys_changing_alternate_version)
-        try {
-            mMediaPlayer?.setDataSource(afd.fileDescriptor, afd.startOffset, afd.length)
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
-        mMediaPlayer?.setOnPreparedListener {
-            isReady = true
-            mMediaPlayer?.start()
-        }
-        mMediaPlayer?.setOnErrorListener { _, _, _ -> false }
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart: ")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop: ")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume: ")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy: ")
+        unbindService(mServiceConnection)
+        mBoundServiceIntent.action = MediaService.ACTION_DESTROY
+
+        startService(mBoundServiceIntent)
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart: ")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause: ")
     }
 }
