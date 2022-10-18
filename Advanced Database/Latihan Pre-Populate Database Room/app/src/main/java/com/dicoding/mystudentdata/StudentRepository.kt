4cedc013daf561e09eb3287b94200c8ce83ee0ac
Dien <dien_kamal@ymail.com>
2022-10-18 20:37:17 +0700

    Latihan Pre-Populate Database Room

---
+++ Advanced Database/Latihan Pre-Populate Database Room/app/src/main/java/com/dicoding/mystudentdata/StudentRepository.kt
@@ -0,0 +1,22 @@
+package com.dicoding.mystudentdata
+
+import androidx.lifecycle.LiveData
+import com.dicoding.mystudentdata.database.*
+import com.dicoding.mystudentdata.helper.InitialDataSource
+
+class StudentRepository(private val studentDao: StudentDao) {
+    fun getAllStudent(): LiveData<List<Student>> = studentDao.getAllStudent()
+//    Many to one
+    fun getAllStudentAndUniversity(): LiveData<List<StudentAndUniversity>> = studentDao.getAllStudentAndUniversity()
+//    One to Many
+    fun getAllUniversityAndStudent(): LiveData<List<UniversityAndStudent>> = studentDao.getAllUniversityAndStudent()
+//    Many to many
+    fun getAllStudentWithCourse(): LiveData<List<StudentWithCourse>> = studentDao.getAllStudentWithCourse()
+
+//    suspend fun insertAllData() {
+//        studentDao.insertStudent(InitialDataSource.getStudents())
+//        studentDao.insertUniversity(InitialDataSource.getUniversities())
+//        studentDao.insertCourse(InitialDataSource.getCourses())
+//        studentDao.insertCourseStudentCrossRef(InitialDataSource.getCourseStudentRelation())
+//    }
+}
