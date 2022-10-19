5f0d00ecbcb7c46ef6f99ff14e2c2825ba94dd32
Dien <dien_kamal@ymail.com>
2022-10-19 10:00:04 +0700

    Cleanup

---
+++ Advanced Database/Latihan Melakukan Sorting dalam Database Room/app/src/main/java/com/dicoding/mystudentdata/StudentRepository.kt
@@ -0,0 +1,27 @@
+package com.dicoding.mystudentdata
+
+import androidx.lifecycle.LiveData
+import com.dicoding.mystudentdata.database.*
+import com.dicoding.mystudentdata.helper.InitialDataSource
+import com.dicoding.mystudentdata.helper.SortType
+import com.dicoding.mystudentdata.helper.SortUtils
+
+class StudentRepository(private val studentDao: StudentDao) {
+    fun getAllStudent(sortType: SortType): LiveData<List<Student>> {
+        val query = SortUtils.getSortedQuery(sortType)
+        return studentDao.getAllStudent(query)
+    }
+//    Many to one
+    fun getAllStudentAndUniversity(): LiveData<List<StudentAndUniversity>> = studentDao.getAllStudentAndUniversity()
+//    One to Many
+    fun getAllUniversityAndStudent(): LiveData<List<UniversityAndStudent>> = studentDao.getAllUniversityAndStudent()
+//    Many to many
+    fun getAllStudentWithCourse(): LiveData<List<StudentWithCourse>> = studentDao.getAllStudentWithCourse()
+
+//    suspend fun insertAllData() {
+//        studentDao.insertStudent(InitialDataSource.getStudents())
+//        studentDao.insertUniversity(InitialDataSource.getUniversities())
+//        studentDao.insertCourse(InitialDataSource.getCourses())
+//        studentDao.insertCourseStudentCrossRef(InitialDataSource.getCourseStudentRelation())
+//    }
+}
